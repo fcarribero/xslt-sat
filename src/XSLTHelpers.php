7abb84d1c90c6d5a9ef702bc162b387f20076517
Francisco Carribero <cfrancisco@advans.mx>
2021-10-23 10:08:50 -0300

    .

---
+++ src/XSLTHelpers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Webneex\XSDSAT;
+
+use DOMDocument;
+use XSLTProcessor;
+
+class XSLTHelpers {
+
+    public static function transformDom($dom, $schema) {
+
+        if (substr($schema, 0, 2) == './') $schema = __DIR__ . '/../files/' . substr($schema, 2);
+
+        $style = new DOMDocument;
+        $style->load($schema, LIBXML_NOCDATA);
+        $xslt = new XSLTProcessor;
+        $xslt->importStylesheet($style);
+
+        return $xslt->transformToXML($dom);
+    }
+
+    public static function transformString($string, $schema) {
+        $dom = new DOMDocument;
+        $dom->loadXML($string);
+
+        return static::transformDom($dom, $schema);
+    }
+}
